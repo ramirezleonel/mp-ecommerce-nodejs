1b08db8fbb16d841fa38cdb2a2002f661e90fc2f
ramirezleonel <ramirezleonel0@gmail.com>
2020-10-29 01:00:37 -0300

    se agregó la redireccion a la pagina de mercadopago

--- pagarmp.js
+++ pagarmp.js
@@ -1,6 +1,8 @@
 var mercadopago = require('mercadopago');
 
-module.exports = () => {
+var url_base = 'localhost:5000'
+module.exports = (req,res) => {
+    console.log(req.body.price);
     mercadopago.configure({
         integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
         access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'
@@ -11,41 +13,40 @@ module.exports = () => {
             "id": 471923173,
             "name": "Lalo Landa",
             "email": "test_user_63274575@testuser.com",
-            "phone": {
-                "area_code": "11",
-                "number": "22223333"
-            },
-            "identification": {
-                "type": "DNI",
-                "number": "12345678"
-            },
+          
             "address": {
                 "street_name": "False",
                 "street_number": 123,
                 "zip_code": "1111"
             }
         },
+        "back_urls": {
+            "success": url_base +"/success",
+            "pending": url_base +"/pending",
+            "failure": url_base +"/failure",
+        },
+        "external_reference": "ramirezleonel0@gmail.com",
         "items": [
             {
-                "id": "item-ID-1234",
-                "title": "Mi producto",
-                "currency_id": "MXN",
-                "picture_url": "https://www.mercadopago.com/org-img/MP3/home/logomp3.gif",
+                "id": 1234,
+                "title": req.body.title,
+                "picture_url": `${req.body.img}`,
                 "description": "Dispositivo móvil de Tienda e-commerce",
-                "category_id": "art",
                 "quantity": 1,
-                "unit_price": 75.76
+                "unit_price": parseInt(req.body.price) 
             }
         ],
 };
     
     
-    mercadopago.preferences.create(preference).then(function(response){
+    mercadopago.preferences.create(preference).then(function(Response){
         // Este valor reemplazará el string "<%= global.id %>" en tu HTML
-        console.log(response);
-        return response;
+        console.log(Response.body);
+        global.init_point = Response.body.id
+        res.redirect(Response.body.init_point)
+     
         }).catch(function(error){
-            return error;
+            console.log(error);
         });
 }
 
