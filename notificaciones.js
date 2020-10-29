e5ea437e57b9bbef346335aeaae51c894234d6cf
ramirezleonel <ramirezleonel0@gmail.com>
2020-10-29 05:53:40 -0300

    se guarda en un txt la notificacion

--- notificaciones.js
+++ notificaciones.js
@@ -1,16 +1,21 @@
 const mercadopago = require ('mercadopago');
-
+var fs = require('fs');
 
 module.exports = (req,res) => {
 // Agrega credenciales
- res.sendStatus(200);
+
   mercadopago.configure({
     integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
     access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'
   });
   
    console.log(req.body);
- 
+   res.sendStatus(200);
+   fs.appendFile('notificacion.txt', req.body, function (err) {
+    if (err) return console.log(err);
+    console.log('Appended!');
+});
+
     switch (req.body.type) {
         case 'payment':
        mercadopago.payment.findById('https://api.mercadopago.com/v1/payments/'+req.body.id).then(function(Response){})
@@ -26,5 +31,5 @@ module.exports = (req,res) => {
           break;
       }
    
-    
+     
 }
